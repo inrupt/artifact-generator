22bc38431a01d3623d9589456daecf1bf7eb753c
Jarlath Holleran <jarlath.holleran@gmail.com>
2019-06-21 10:02:33 +0100

    loads vocab and vocab extensions, merges them together and passes it to the handlebars template. Also, refactored and added tests

--- app.ts
+++ app.ts
@@ -5,59 +5,46 @@ const rdf = require('rdf-ext')
 const { LitUtils } = require('lit-vocab-term')
 
 
-fs.readFile('templates/template.hbs', function read(err, data) {
-    if (err) {
-        throw err;
-    }
+const { RDF, RDFS, SCHEMA, OWL } = require('vocab-lit')
 
-	var template = Handlebars.compile(data.toString());
-	
+function processVocab() {
 
-	const classes = [];
-	const properties = [];
+	fs.readFile('templates/template.hbs', function read(err, data) {
+		if (err) {
+			throw err;
+		}
 
-	const result = {};
-	result.classes = classes;
-	result.properties = properties;
-	result.namespace = 'http://schema.org/'; //TODO read this from the data
+		var template = Handlebars.compile(data.toString());
 
+		LitUtils.loadTurtleFile('./vocabs/schema.ttl', (dataset) => {
+			LitUtils.loadTurtleFile('./vocabs/schema-ext.ttl', (extensionDataset) => {
 
-	LitUtils.loadTurtleFile('C:/Users/holleranj/development/LIT/vocab/rdf/OntologyDirMarker/schema-ext.ttl', function (data) {
-	    
-	    data.match(null, null, rdf.namedNode('http://www.w3.org/2000/01/rdf-schema#Class')).filter((quad) => {
-	    	classes.push(handleTerms(data, quad));
-	    });
+				const result = buildTemplateInput(dataset, extensionDataset);
 
-	    data.match(null, null, rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#Property')).filter((quad) => {
-			
-			properties.push(handleTerms(data, quad));
-	    });
+				var contents = template(result);
 
-
-
-		//console.log('Result : ' + JSON.stringify(result, null, ' '));
-
-
-		var contents = template(result);
-
-		fs.writeFile('generated/schema-ext.ts', contents, err => {
-		    if (err) {
-		        return console.error('Failed to store template: ${err.message}.');
-		    }
-		    console.log('Saved template!');
+				fs.writeFile('generated/schema-ext.ts', contents, err => {
+					if (err) {
+						return console.error('Failed to store template: ${err.message}.');
+					}
+					console.log('Saved template!');
+				});
+				
+			});
 		});
 
-	});
 
-});
+	});
+}
 
 function handleTerms(data, quad) {
+
 	const labels = [];
 	data.match(quad.subject, rdf.namedNode('http://www.w3.org/2000/01/rdf-schema#label'), null).filter((subQuad) => {
 		add(labels, subQuad);
 	});
 
-	
+
 	const alternateName = [];
 	data.match(quad.subject, rdf.namedNode('http://schema.org/alternateName'), null).filter((subQuad) => {
 		add(alternateName, subQuad);
@@ -71,7 +58,7 @@ function handleTerms(data, quad) {
 	return {name: labels[0].value,
 				comment: comments[0].value,
 				labels: labels,
-				alternateName: alternateName,
+				alternateNames: alternateName,
 				comments: comments};
 }
 
@@ -81,3 +68,62 @@ function add(array, quad) {
 		language: quad.object.language
 	});
 }
+
+function buildTemplateInput(vocabData, vocabExtentionData) {
+
+	const fullData = vocabData.merge(vocabExtentionData);
+
+	const classes = [];
+	const properties = [];
+
+	const result = {};
+	result.classes = classes;
+	result.properties = properties;
+	result.namespace = 'http://schema.org/'; //TODO read this from the data
+	result.ontologyNameUppercase = "SCHEMA_EXT"
+
+
+	const ontologyStatements = vocabExtentionData.match(null, rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+		rdf.namedNode('http://www.w3.org/2002/07/owl#Ontology')).toArray();
+	// if (ontologyStatements.length != 1) {
+	// 	throw new Error(`Must contain only 1 ontology, but found [${ontologyStatements.length}], i.e. ${ontologyStatements.toString()}]`)
+	// }
+
+	//const ontologySubject = ontologyStatements[0].subject.value
+	//console.log(`ontologySubject: [${ontologySubject}]`);
+
+
+	const terms = vocabExtentionData.filter((quad) => {
+		return quad.subject.value !== OWL.Ontology;
+	})
+
+	//console.log(`term: [${terms.toString()}]`);
+
+	const termSubjects = [];
+	terms.filter((quad) => {
+		//console.log(quad.subject.value);
+		termSubjects.push(quad.subject.value);
+	});
+
+	const subjectSet = [...new Set(termSubjects)]
+	//console.log(termSubjects.length);
+
+	//console.log(`term subjects: [${subjectSet}]`);
+
+	subjectSet.forEach(function(entry) {
+		//console.log(entry);
+
+		fullData.match(entry, null, rdf.namedNode('http://www.w3.org/2000/01/rdf-schema#Class')).filter((quad) => {
+			classes.push(handleTerms(fullData, quad));
+		});
+
+		fullData.match(entry, null, rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#Property')).filter((quad) => {
+			properties.push(handleTerms(fullData, quad));
+		});
+	});
+
+	return result;
+}
+
+module .exports.processVocab = processVocab;
+module.exports.buildTemplateInput = buildTemplateInput;
---
+++ test/generator.test.js
@@ -0,0 +1,91 @@
+'use strict'
+
+const rdf = require('rdf-ext')
+
+const { RDF, RDFS, SCHEMA } = require('vocab-lit')
+
+const gen = require('../app.ts')
+
+const chai = require('chai')
+chai.use(require('chai-string'));
+const expect = chai.expect
+
+
+
+// const person = rdf.namedNode('http://schema.org/Person')
+// const givenName = rdf.namedNode('http://schema.org/givenName')
+// const familyName = rdf.namedNode('http://schema.org/familyName')
+
+const dataset = rdf.dataset().addAll([
+  rdf.quad(SCHEMA.Person, RDF.type, RDFS.Class),
+  rdf.quad(SCHEMA.Person, RDFS.label, rdf.literal('Person', 'fr')),
+  rdf.quad(SCHEMA.Person, RDFS.comment, rdf.literal('Person dead of alive', 'en')),
+
+
+  rdf.quad(SCHEMA.givenName, RDF.type, RDF.Property),
+  rdf.quad(SCHEMA.givenName, RDFS.label, rdf.literal('givenName', '')),
+  rdf.quad(SCHEMA.givenName, RDFS.comment, rdf.literal('A given name is the first name of a person.', 'en')),
+
+  rdf.quad(SCHEMA.familyName, RDF.type, RDF.Property),
+  rdf.quad(SCHEMA.familyName, RDFS.label, rdf.literal('familyName', 'fr')),
+  rdf.quad(SCHEMA.familyName, RDFS.comment, rdf.literal('A family name is the last name of a person.', 'en')),
+
+]);
+
+const datasetExtension = rdf.dataset().addAll([
+  rdf.quad(SCHEMA.Person, SCHEMA.alternateName, rdf.literal('Person-fr', 'fr')),
+  rdf.quad(SCHEMA.Person, SCHEMA.alternateName, rdf.literal('Person-de', 'de')),
+  rdf.quad(SCHEMA.Person, SCHEMA.alternateName, rdf.literal('Person-es', 'es')),
+  rdf.quad(SCHEMA.Person, RDFS.comment, rdf.literal('Person dead of alive fr', 'fr')),
+  rdf.quad(SCHEMA.Person, RDFS.comment, rdf.literal('Person dead of alive de', 'de')),
+  rdf.quad(SCHEMA.Person, RDFS.comment, rdf.literal('Person dead of alive es', 'es')),
+
+  rdf.quad(SCHEMA.givenName, SCHEMA.alternateName, rdf.literal('Given Name', 'en')),
+  rdf.quad(SCHEMA.givenName, SCHEMA.alternateName, rdf.literal('Given Name-fr', 'fr')),
+  rdf.quad(SCHEMA.givenName, SCHEMA.alternateName, rdf.literal('Given Name-de', 'de')),
+  rdf.quad(SCHEMA.givenName, SCHEMA.alternateName, rdf.literal('Given Name-es', 'es')),
+  rdf.quad(SCHEMA.givenName, RDFS.comment, rdf.literal('Given name of a person fr', 'fr')),
+  rdf.quad(SCHEMA.givenName, RDFS.comment, rdf.literal('Given name of a person de', 'de')),
+  rdf.quad(SCHEMA.givenName, RDFS.comment, rdf.literal('Given name of a person es', 'es')),
+]);
+
+
+describe ('LIT JS unit tests', () => {
+  // const aliceIriAsString = 'https://alice.example.org/profile#me'
+  // const alice = rdf.namedNode(aliceIriAsString)
+
+  beforeEach(() => {
+    // delete process.env.IRI_HINT_APPLICATION
+    // delete process.env.DATA_SERVER_SOLID
+  })
+
+  describe ('Building the Template input', () => {
+    it ('should create a simple JSON object with all the fields', () => {
+      // const username = 'TestUser'
+      // const webId = LitUtils.createWebId(username)
+      // expect(webId.value).to.include('https://', username, LitUtils.DEFAULT_WEDID_SERVER_DOMAIN, '#')
+
+      const result = gen.buildTemplateInput(dataset, datasetExtension);
+      expect(result.namespace).to.equal("http://schema.org/");
+      expect(result.ontologyNameUppercase).to.equal("SCHEMA_EXT");
+      expect(result.classes[0].name).to.equal("Person");
+      expect(result.classes[0].comment).to.equal("Person dead of alive");
+      expect(result.classes[0].labels[0].value).to.equal("Person");
+      expect(result.classes[0].alternateNames[0].value).to.equal("Person-fr");
+      expect(result.classes[0].alternateNames[1].value).to.equal("Person-de");
+      expect(result.classes[0].alternateNames[2].value).to.equal("Person-es");
+
+      expect(result.properties[0].name).to.equal("givenName");
+      expect(result.properties[0].comment).to.equal("A given name is the first name of a person.");
+      expect(result.properties[0].labels[0].value).to.equal("givenName");
+      expect(result.properties[0].alternateNames[0].value).to.equal("Given Name");
+      expect(result.properties[0].alternateNames[1].value).to.equal("Given Name-fr");
+      expect(result.properties[0].alternateNames[2].value).to.equal("Given Name-de");
+      expect(result.properties[0].alternateNames[3].value).to.equal("Given Name-es");
+    })
+
+    it ('Should generate using environment value', () => {
+      gen.processVocab();
+    })
+  })
+})
