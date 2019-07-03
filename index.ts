bcf2d2cf658c3d7d0821b14889362e2e699e6637
Jarlath Holleran <jarlathh@inrupt.com>
2019-07-03 14:55:31 +0100

    taking in input ontologies and subject only extensions on command line

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 const gen = require('./src/generator.js');
 
 
-//gen.generate(['./vocabs/schema.ttl', './vocabs/schema-inrupt-ext.ttl'], '1.0.0', './vocabs/schema-inrupt-ext.ttl');
+const argv = require('yargs')
+    .array('input')
+    .argv
 
 
-gen.generate(['./vocabs/vcard.ttl', './vocabs/vcard-inrupt-ext.ttl'], '1.0.0', './vocabs/vcard-inrupt-ext.ttl');
+gen.generate(argv.input || [], argv.mversion, argv.subjects);
 
-
-//gen.generate(['http://www.w3.org/2002/07/owl'], '1.0.0');
 
 
