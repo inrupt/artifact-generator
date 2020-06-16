a0f036b039ec0f2a8250bf6306e8df8671c2cb20
Pat McBennett <patm@inrupt.com>
2020-06-16 11:04:11 +0100

    Renamed scope back to inrupt, use lit-term consistently (#334)
    * Renamed scope back to inrupt, use lit-term consistently
    
    * Remove reference to dependency from README file

--- src/generator/ConfigFileGenerator.test.js
+++ src/generator/ConfigFileGenerator.test.js
@@ -60,7 +60,7 @@ const COMPLETE_VOCAB = {
 
 const COMPLETE_CONFIG = {
   artifactName: ARTIFACT_NAME,
-  generatorName: "@solid/lit-artifact-generator",
+  generatorName: "@inrupt/lit-artifact-generator",
   artifactGeneratorVersion: packageDotJson.version,
   artifactToGenerate: [COMPLETE_JAVA_ARTIFACT],
   vocabList: [COMPLETE_VOCAB],
