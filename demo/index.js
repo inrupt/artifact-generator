377fc4510d460df3e5baff449add213aa78ebd20
Andy Davison <adavison@and.digital>
2021-07-05 17:04:02 +0100

    Fix example getting comment in language

--- demo/index.js
+++ demo/index.js
@@ -7,4 +7,4 @@ console.log(`Our vocabulary describes it as:`);
 console.log(`"${PET_ROCK.shinyness.comment}"\n`);
 
 console.log(`Or in Spanish (our Pet Rock vocab has Spanish translations!):`);
-console.log(`"${PET_ROCK.shinyness.commentInLang('es')}"`);
+console.log(`"${PET_ROCK.shinyness.asLanguage('es').comment}"`);
