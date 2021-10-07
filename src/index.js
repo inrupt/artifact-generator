9a4f6c2c3e7a9b191481e4144fc6383d39847e8a
pmcb55 <patm@inrupt.com>
2021-10-07 17:02:36 +0100

    Add comment for stdin

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const processCommandLine = require("./commandLineProcessor");
 
 try {
   processCommandLine(true, process.argv.slice(2)).then((argv) => {
+    // TODO: This watcher clean up should really be inside the watcher code
+    //  itself, but for that we need to mock `process.stdin` (to emulate the
+    //  developer hitting the 'Enter' key), so just do it here for now!
     if (argv._[0] === "watch") {
       process.stdin.on("data", () => {
         argv.unwatchFunction();
